5b5a4039f9fc4f3d55f93a654b9369af96cd6f34
ijvanl <ishvanlooy@gmail.com>
2024-06-28 23:03:31 +1000

    first commit

---
+++ fill.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var stacks = [
+	document.getElementById("stack1"),
+	document.getElementById("stack2"),
+	document.getElementById("stack3")
+];
+
+function fillStacks(text) {
+	var files = text.split(/\n/);
+	for (var i = 0; i < files.length; i++) { 
+		var image = document.createElement("img");
+		image.src = "images/" + files[i];
+		stacks[i % 3].appendChild(image);
+	}
+}
+
+fetch("images/images.txt").then((response) => {
+	if (!response.ok) throw new Error(`HTTP error: ${response.status}`);
+	return response.text();
+}).then((text) => fillStacks(text))
